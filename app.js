1803864f4af646a07e7951f298b3b0ae2d36401b
David Gurovich <david.gurovich@outlook.com>
2022-08-17 21:05:26 -0700

    clear button resets the board

--- app.js
+++ app.js
@@ -5,8 +5,43 @@ function makeRows(rows, cols) {
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
-        cell.innerText = (c + 1)
+        //cell.innerText = (c + 1)
+        cell.addEventListener('mouseover', randomColors )
         container.appendChild(cell).className = "grid-item";
+        
     }
 }
 makeRows(16, 16)
+
+// buttons for eraser and clear 
+const interface = document.getElementById('interface')
+const eraser = document.createElement('button');
+eraser.innerText = 'Erase';
+interface.appendChild(eraser);
+
+const clearAll = document.createElement('button');
+clearAll.innerText = "Clear";
+clearAll.addEventListener('click', handleReset)
+interface.appendChild(clearAll);
+
+function random(number){
+    return Math.floor(Math.random() * (number + 1));
+}
+
+function randomColors (e) {
+    let color = `rgb(${[0, 0, 0].map(channel => {
+        return Math.floor(Math.random() * 256);
+      }).join(',')})`
+      Object.assign(e.target.style, {
+        backgroundColor : color,
+        opacity : 0.75
+      });
+    }
+
+
+function handleReset(e) {
+    while (container.firstChild){
+        container.removeChild(container.lastChild)
+    }
+    makeRows(16, 16)
+}
